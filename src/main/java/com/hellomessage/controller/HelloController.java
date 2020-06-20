fed65cca6e21d6615c49d533e300406395250d40
seema1611 <seemarajpure16@gmail.com>
2020-06-21 02:26:04 +0530

    ADDED: Printing name using different http method

--- src/main/java/com/hellomessage/controller/HelloController.java
+++ src/main/java/com/hellomessage/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.hellomessage.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping(value = {"", "/", "/home"})
+    @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello World";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name ) {
+        return "Hello " +name+ "!";
     }
 }
